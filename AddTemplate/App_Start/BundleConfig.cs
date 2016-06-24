0651b8e4d15dd0b4848b7a7cd862f8f1fce80979
rsen253 <rsen253@gmail.com>
2016-06-24 13:52:10 +0530

    Added bootstarp template and modified the layout

---
+++ AddTemplate/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AddTemplate
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/classie.js",
+                        "~/Scripts/cbpAnimatedHeader.js",
+                        "~/Scripts/jqBootstrapValidation.js",
+                        "~/Scripts/contact_me.js",
+                        "~/Scripts/freelancer.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/freelancer.css",
+                      "~/font-awesome/css/font-awesome.min.css"));
+        }
+    }
+}
